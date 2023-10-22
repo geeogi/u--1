97cfaa85139993bec102f99f97ac36d969253690
geeogi <george6bennett@gmail.com>
2023-10-22 18:41:01 +0100

    update

--- web/page-staking-build.js
+++ web/page-staking-build.js
@@ -6,13 +6,37 @@ async function main() {
     opWstETHBridge: "0x76943C0D61395d8F2edF9060e1533529cAe05dE6",
     stETH: "0xae7ab96520de3a18e5e111b5eaab095312d7fe84",
     wstETH: "0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0",
+    cbETH: "0xBe9895146f7AF43049ca1c1AE358B0541Ea49704",
+    rETH: "0xae78736cd615f374d3085123a210448e74fc6393",
     curveStETH: "0xdc24316b9ae028f1497c275eb9192a3ea0f67022",
+    eigenlayerStETH: "0x93c4b944D05dfe6df7645A86cd2206016c51564D",
+    eigenlayerCbETH: "0x54945180dB7943c0ed0FEE7EdaB2Bd24620256bc",
+    eigenlayerRETH: "0x1BeE69b7dFFfA4E2d53C2a2Df135C388AD25dCD2",
+    divsStETH: "0x1ce8aAfb51e79F6BDc0EF2eBd6fD34b00620f6dB",
+    lybraStETH: "0xa980d4c0C2E48d305b582AA439a3575e3de06f0E",
+    aaveStETH: "0x1982b2F5814301d4e9a8b0201555376e62F82428",
+    unstETH: "0x889edC2eDab5f40e902b864aD4d7AdE8E412F9B1",
   };
 
-  const { stETH, wstETH, curveStETH, arbWstETHBridge, opWstETHBridge } =
-    CONTRACTS;
+  const {
+    stETH,
+    wstETH,
+    cbETH,
+    rETH,
+    curveStETH,
+    arbWstETHBridge,
+    opWstETHBridge,
+    eigenlayerStETH,
+    eigenlayerCbETH,
+    eigenlayerRETH,
+    divsStETH,
+    lybraStETH,
+    aaveStETH,
+    unstETH,
+  } = CONTRACTS;
 
   const stETHSupply = await EthereumRPC.totalSupply(stETH);
+  // cbETH rETH supply
   const stETHApr = await LidoAPI.stETHApr();
   const stETHwstETHExchange = await EthereumRPC.getStETHWstETHExchangeRate();
   const wstETHSupply = await EthereumRPC.totalSupply(wstETH);
@@ -22,6 +46,14 @@ async function main() {
   const crvStETHLpETH = await EthereumRPC.ethBalance(curveStETH);
   const ethPrice = await EthereumRPC.ethPrice();
   const stETHPrice = await EthereumRPC.stETHPrice();
+  const elStETHBalance = await EthereumRPC.balanceOf(stETH, eigenlayerStETH);
+  const elCbETHBalance = await EthereumRPC.balanceOf(cbETH, eigenlayerCbETH);
+  const elRETHBalance = await EthereumRPC.balanceOf(rETH, eigenlayerRETH);
+  const divaStETHBalance = await EthereumRPC.balanceOf(stETH, divsStETH);
+  const lybraStETHBalance = await EthereumRPC.balanceOf(stETH, lybraStETH);
+  // swell, obol, ssv
+  const aaveStETHBalance = await EthereumRPC.balanceOf(stETH, aaveStETH);
+  const unstETHBalance = await EthereumRPC.balanceOf(stETH, unstETH);
 
   console.log("stETH", stETHSupply);
   console.log("stETH APR", stETHApr);
@@ -33,6 +65,13 @@ async function main() {
   console.log("crv ETH balance", crvStETHLpETH);
   console.log("ETH price", ethPrice);
   console.log("stETH price", stETHPrice);
+  console.log("EL stETH balance", elStETHBalance);
+  console.log("EL cbETH balance", elCbETHBalance);
+  console.log("EL rETH balance", elRETHBalance);
+  console.log("DIVA stETH balance", divaStETHBalance);
+  console.log("Lybra stETH balance", lybraStETHBalance);
+  console.log("Aave stETH balance", aaveStETHBalance);
+  console.log("stETH withdrawal queue", unstETHBalance);
 }
 
 main();
