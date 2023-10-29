037f671987c1defd00629b63053f49d6467daa2f
geeogi <george6bennett@gmail.com>
2023-10-29 16:57:37 +0000

    update

--- web/page-staking-build.js
+++ web/page-staking-build.js
@@ -6,63 +6,136 @@ async function main() {
 
   const values = {
     // Add other balances for aave v3 (eth, op, arb, cbETH)
-    aaveStETHBalance: RPC.balanceOf(CONTRACTS.stETH, CONTRACTS.aaveV2StETH),
-    cbETHSupply: RPC.totalSupply(CONTRACTS.cbETH),
-    crvStETHLpETH: RPC.ethBalance(CONTRACTS.curveStETH),
-    crvStETHLpStETH: RPC.balanceOf(CONTRACTS.stETH, CONTRACTS.curveStETH),
-    divaStETHBalance: RPC.balanceOf(CONTRACTS.stETH, CONTRACTS.divsStETH),
-    elCbETHBalance: RPC.balanceOf(CONTRACTS.cbETH, CONTRACTS.eigenlayerCbETH),
-    elRETHBalance: RPC.balanceOf(CONTRACTS.rETH, CONTRACTS.eigenlayerRETH),
-    elStETHBalance: RPC.balanceOf(CONTRACTS.stETH, CONTRACTS.eigenlayerStETH),
-    ethPrice: RPC.ethPrice(),
-    frxETHSupply: RPC.totalSupply(CONTRACTS.frxETH),
-    lybraStETHBalance: RPC.balanceOf(CONTRACTS.stETH, CONTRACTS.lybraStETH),
-    rETHSupply: RPC.totalSupply(CONTRACTS.rETH),
-    sETH2Supply: RPC.totalSupply(CONTRACTS.sETH2),
-    sfrxETHSupply: RPC.totalSupply(CONTRACTS.sfrxETH),
-    stETHBufferedEther: RPC.getLidoBufferedEther(),
-    stETHOperators: RPC.getLidoNodeOperatorsCount(),
-    stETHPrice: RPC.stETHPrice(),
-    stETHSupply: RPC.totalSupply(CONTRACTS.stETH),
-    stETHwstETHExchange: RPC.getStETHWstETHExchangeRate(),
-    swETHSupply: RPC.totalSupply(CONTRACTS.swETH),
-    unstETHBalance: RPC.balanceOf(CONTRACTS.stETH, CONTRACTS.unstETH),
-    wstETHSupply: RPC.totalSupply(CONTRACTS.wstETH),
-    wstETHSupplyArb: RPC.balanceOf(CONTRACTS.wstETH, CONTRACTS.arbWstETHBridge),
-    wstETHSupplyOp: RPC.balanceOf(CONTRACTS.wstETH, CONTRACTS.opWstETHBridge),
+    aaveV2StETHBalance: {
+      call: RPC.balanceOf(CONTRACTS.stETH, CONTRACTS["Aave V2 stETH"]),
+      description: "Balance of stETH in Aave V2",
+    },
+    cbETHSupply: {
+      call: RPC.totalSupply(CONTRACTS.cbETH),
+      description: "Total supply of cbETH",
+    },
+    crvStETHLpETH: {
+      call: RPC.ethBalance(CONTRACTS["Curve stETH"]),
+      description: "Balance of ETH in Curve stETH/ETH",
+    },
+    crvStETHLpStETH: {
+      call: RPC.balanceOf(CONTRACTS.stETH, CONTRACTS["Curve stETH"]),
+      description: "Balance of stETH in Curve stETH/ETH",
+    },
+    divaStETHBalance: {
+      call: RPC.balanceOf(CONTRACTS.stETH, CONTRACTS["Diva stETH"]),
+      description: "Balance of stETH in Diva",
+    },
+    elCbETHBalance: {
+      call: RPC.balanceOf(CONTRACTS.cbETH, CONTRACTS["EigenLayer cbETH"]),
+      description: "Balance of cbETH in Eigenlayer",
+    },
+    elRETHBalance: {
+      call: RPC.balanceOf(CONTRACTS.rETH, CONTRACTS["EigenLayer rETH"]),
+      description: "Balance of rETH in Eigenlayer",
+    },
+    elStETHBalance: {
+      call: RPC.balanceOf(CONTRACTS.stETH, CONTRACTS["EigenLayer stETH"]),
+      description: "Balance of stETH in Eigenlayer",
+    },
+    ethPrice: {
+      call: RPC.ethPrice(),
+      description: "Current price of ETH",
+    },
+    frxETHSupply: {
+      call: RPC.totalSupply(CONTRACTS.frxETH),
+      description: "Total supply of Frax Ether (frxETH)",
+    },
+    lybraStETHBalance: {
+      call: RPC.balanceOf(CONTRACTS.stETH, CONTRACTS["Lybra stETH"]),
+      description: "Balance of stETH in Lybra",
+    },
+    rETHSupply: {
+      call: RPC.totalSupply(CONTRACTS.rETH),
+      description: "Total supply of Rocket Pool ETH (rETH)",
+    },
+    sETH2Supply: {
+      call: RPC.totalSupply(CONTRACTS.sETH2),
+      description: "Total supply of Stakewise sETH2",
+    },
+    sfrxETHSupply: {
+      call: RPC.totalSupply(CONTRACTS.sfrxETH),
+      description: "Total supply of staked Frax Ether (sfrxETH)",
+    },
+    stETHBufferedEther: {
+      call: RPC.getLidoBufferedEther(),
+      description:
+        "ETH in the stETH buffer to be deposited in the beacon chain or withdrawn",
+    },
+    stETHOperators: {
+      call: RPC.getLidoNodeOperatorsCount(),
+      description: "Number of node operators in the Lido stETH protocol",
+    },
+    stETHPrice: {
+      call: RPC.stETHPrice(),
+      description: "Current price of stETH/ETH",
+    },
+    stETHSupply: {
+      call: RPC.totalSupply(CONTRACTS.stETH),
+      description: "Total supply of Lido stETH",
+    },
+    stETHwstETHExchange: {
+      call: RPC.getStETHWstETHExchangeRate(),
+      description: "Exchange rate between stETH and wstETH",
+    },
+    swETHSupply: {
+      call: RPC.totalSupply(CONTRACTS.swETH),
+      description: "Total supply of Swell swETH",
+    },
+    unstETHBalance: {
+      call: RPC.balanceOf(CONTRACTS.stETH, CONTRACTS.unstETH),
+      description: "stETH waiting in the Lido withdrawal queue",
+    },
+    wstETHSupply: {
+      call: RPC.totalSupply(CONTRACTS.wstETH),
+      description: "Total supply of Lido wstETH",
+    },
+    wstETHSupplyArb: {
+      call: RPC.balanceOf(CONTRACTS.wstETH, CONTRACTS["Arbitrum ERC20 bridge"]),
+      description: "Balance of wstETH in the Arbitrum ERC-20 bridge",
+    },
+    wstETHSupplyOp: {
+      call: RPC.balanceOf(CONTRACTS.wstETH, CONTRACTS["OP ERC20 Bridge"]),
+      description: "Balance of wstETH in the Optimism wstETH bridge",
+    },
   };
 
   await Promise.all(
     Object.keys(values).map((key) =>
-      values[key].then?.((resolvedCall) => {
-        values[key] = resolvedCall;
+      values[key].call.then?.((result) => {
+        values[key].result = result;
       })
     )
   );
 
   let htmlContent = fs.readFileSync("page-staking-template.html", "utf8");
 
-  Object.entries(values).forEach(([key, call]) => {
+  Object.entries(values).forEach(([key, { description, result }]) => {
     const value =
-      typeof call.value === "number"
-        ? call.value > 9999
-          ? Math.round(call.value).toLocaleString()
-          : call.value > 1
-          ? Math.round(call.value * 100) / 100
-          : Math.round(call.value * 100000) / 100000
-        : call.value;
+      typeof result.value === "number"
+        ? result.value > 9999
+          ? Math.round(result.value).toLocaleString()
+          : result.value > 1
+          ? Math.round(result.value * 100) / 100
+          : Math.round(result.value * 100000) / 100000
+        : result.value;
 
-    const methodName = RPC.getName(call.methodSignature);
-    const contractName = RPC.getName(call.contractAddress);
-    const arg0 = call.args?.[0];
+    const methodName = RPC.getName(result.methodSignature);
+    const contractName = RPC.getName(result.contractAddress);
+    const arg0 = result.args?.[0];
     const arg0Name = RPC.getName(arg0);
-    const arg1 = call.args?.[1];
+    const arg1 = result.args?.[1];
     const arg1Name = RPC.getName(arg1);
 
     htmlContent = htmlContent.replaceAll(
       `{${key}}`,
       [
-        `<span title="${call.value}">${value}</span>`,
+        `<span title="${result.value}">${value}</span>`,
         "&nbsp;",
         `<button
           onclick="document.getElementById('dialog-${key}').showModal()"
@@ -70,13 +143,14 @@ async function main() {
           ⓘ
         </button>`,
         `<dialog id="dialog-${key}">
-          <p><b>contract</b>: ${contractName} (${call.contractAddress})</p>
-          <p><b>method</b>: ${methodName} (${call.methodSignature})</p>
+          <p><b>description</b>: ${description}</p>
+          <p><b>contract</b>: ${contractName} (${result.contractAddress})</p>
+          <p><b>method</b>: ${methodName} (${result.methodSignature})</p>
           ${arg0 ? `<p><b>arg0</b>: ${arg0Name}  (${arg0})</p>` : ""}
           ${arg1 ? `<p><b>arg1</b>: ${arg1Name}  (${arg1})</p>` : ""}
           <p><b>result</b>: ${value}</p>
           <form method="dialog">
-            <button>OK</button>
+            <button>CLOSE</button>
           </form>
         </dialog>`,
       ].join("")
--- web/page-staking-utils.js
+++ web/page-staking-utils.js
@@ -3,25 +3,25 @@ export const RPC = {
     "https://eth-mainnet.g.alchemy.com/v2/VjRG6l7jUiq9cJNzPDoLIw2lFz-pz_Ra",
 
   CONTRACTS: {
-    aaveV2StETH: "0x1982b2F5814301d4e9a8b0201555376e62F82428",
-    arbWstETHBridge: "0x0F25c1DC2a9922304f2eac71DCa9B07E310e8E5a",
+    "Aave V2 stETH": "0x1982b2F5814301d4e9a8b0201555376e62F82428",
+    "Arbitrum ERC20 bridge": "0x0F25c1DC2a9922304f2eac71DCa9B07E310e8E5a",
+    "Chainlink ETH/USD": "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419",
+    "Chainlink stETH/ETH": "0x86392dC19c0b719886221c78AB11eb8Cf5c52812",
+    "Curve stETH": "0xdc24316b9ae028f1497c275eb9192a3ea0f67022",
+    "Diva stETH": "0x1ce8aAfb51e79F6BDc0EF2eBd6fD34b00620f6dB",
+    "EigenLayer cbETH": "0x54945180dB7943c0ed0FEE7EdaB2Bd24620256bc",
+    "EigenLayer rETH": "0x1BeE69b7dFFfA4E2d53C2a2Df135C388AD25dCD2",
+    "EigenLayer stETH": "0x93c4b944D05dfe6df7645A86cd2206016c51564D",
+    "Lido Node Registry": "0x55032650b14df07b85bF18A3a3eC8E0Af2e028d5",
+    "Lybra stETH": "0xa980d4c0C2E48d305b582AA439a3575e3de06f0E",
+    "OP ERC20 Bridge": "0x76943C0D61395d8F2edF9060e1533529cAe05dE6",
+    Multicall: "0xeefBa1e63905eF1D7ACbA5a8513c70307C1cE441",
     cbETH: "0xBe9895146f7AF43049ca1c1AE358B0541Ea49704",
-    curveStETH: "0xdc24316b9ae028f1497c275eb9192a3ea0f67022",
-    divsStETH: "0x1ce8aAfb51e79F6BDc0EF2eBd6fD34b00620f6dB",
-    eigenlayerCbETH: "0x54945180dB7943c0ed0FEE7EdaB2Bd24620256bc",
-    eigenlayerRETH: "0x1BeE69b7dFFfA4E2d53C2a2Df135C388AD25dCD2",
-    eigenlayerStETH: "0x93c4b944D05dfe6df7645A86cd2206016c51564D",
-    ethMulticall: "0xeefBa1e63905eF1D7ACbA5a8513c70307C1cE441",
-    ethUsdChainlink: "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419",
     frxETH: "0x5e8422345238f34275888049021821e8e08caa1f",
-    lidoNodeOperatorsRegistry: "0x55032650b14df07b85bF18A3a3eC8E0Af2e028d5",
-    lybraStETH: "0xa980d4c0C2E48d305b582AA439a3575e3de06f0E",
-    opWstETHBridge: "0x76943C0D61395d8F2edF9060e1533529cAe05dE6",
     rETH: "0xae78736cd615f374d3085123a210448e74fc6393",
     sETH2: "0xFe2e637202056d30016725477c5da089Ab0A043A",
     sfrxETH: "0xac3e018457b222d93114458476f3e3416abbe38f",
     stETH: "0xae7ab96520de3a18e5e111b5eaab095312d7fe84",
-    stETHETHChainlink: "0x86392dC19c0b719886221c78AB11eb8Cf5c52812",
     swETH: "0xf951e335afb289353dc249e82926178eac7ded78",
     unstETH: "0x889edC2eDab5f40e902b864aD4d7AdE8E412F9B1",
     wstETH: "0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0",
@@ -107,7 +107,7 @@ export const RPC = {
   },
 
   async ethBalance(address) {
-    const target = this.CONTRACTS.ethMulticall;
+    const target = this.CONTRACTS.Multicall;
     const signature = this.SIGNATURES.getEthBalance;
     const args = [address];
     const result = await this.callContractMethod(target, signature, args);
@@ -116,7 +116,7 @@ export const RPC = {
   },
 
   async ethPrice() {
-    const target = this.CONTRACTS.ethUsdChainlink;
+    const target = this.CONTRACTS["Chainlink ETH/USD"];
     const signature = this.SIGNATURES.latestAnswer;
     const result = await this.callContractMethod(target, signature);
     result.value = parseInt(result.value, 16) / 10 ** 8;
@@ -124,7 +124,7 @@ export const RPC = {
   },
 
   async stETHPrice() {
-    const target = this.CONTRACTS.stETHETHChainlink;
+    const target = this.CONTRACTS["Chainlink stETH/ETH"];
     const signature = this.SIGNATURES.latestAnswer;
     const result = await this.callContractMethod(target, signature);
     result.value = parseInt(result.value, 16) / 10 ** 18;
@@ -140,7 +140,7 @@ export const RPC = {
   },
 
   async getLidoNodeOperatorsCount() {
-    const target = this.CONTRACTS.lidoNodeOperatorsRegistry;
+    const target = this.CONTRACTS["Lido Node Registry"];
     const signature = this.SIGNATURES.getNodeOperatorsCount;
     const result = await this.callContractMethod(target, signature);
     result.value = parseInt(result.value, 16);
