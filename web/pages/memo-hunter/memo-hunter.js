b92707adab992f63727f142f0fc4352ccb5c1dc6
geeogi <george6bennett@gmail.com>
2023-12-21 22:50:29 +0000

    update

--- web/pages/memo-hunter/memo-hunter.js
+++ web/pages/memo-hunter/memo-hunter.js
@@ -1,46 +1,50 @@
 async function hunt() {
-  const mainEl = document.getElementById("main");
-  const footerEl = document.getElementById("footer");
+  try {
+    const mainEl = document.getElementById("main");
+    const footerEl = document.getElementById("footer");
 
-  footerEl.innerText = "hunting blocks...";
+    footerEl.innerText = "hunting blocks...";
 
-  const base = "https://celestia.api.explorers.guru/api/v1";
-  const exploreTx = "https://celestia.explorers.guru/transaction";
-  const blocksResponse = await fetch(`${base}/blocks?limit=10`);
-  const blocks = await blocksResponse.json();
+    const base = "https://celestia.api.explorers.guru/api/v1";
+    const exploreTx = "https://celestia.explorers.guru/transaction";
+    const blocksResponse = await fetch(`${base}/blocks?limit=10`);
+    const blocks = await blocksResponse.json();
 
-  footerEl.innerText = "hunting txs...";
+    footerEl.innerText = "hunting txs...";
 
-  blocks.data.forEach(async (block) => {
-    const blockEl = document.createElement("div");
-    blockEl.id = block.height;
-    blockEl.style.backgroundColor = "#f0f0f0";
-    blockEl.innerText = `block: ${block.height}`;
-    mainEl.appendChild(blockEl);
+    blocks.data.forEach(async (block) => {
+      const blockEl = document.createElement("div");
+      blockEl.id = block.height;
+      blockEl.style.backgroundColor = "#f0f0f0";
+      blockEl.innerText = `block: ${block.height}`;
+      mainEl.appendChild(blockEl);
 
-    const txsResponse = await fetch(`${base}/blocks/${block.height}/txs`);
-    const txs = await txsResponse.json();
+      const txsResponse = await fetch(`${base}/blocks/${block.height}/txs`);
+      const txs = await txsResponse.json();
 
-    txs.data.forEach(async (txInfo) => {
-      const txEl = document.createElement("div");
-      txEl.id = txInfo.hash;
-      txEl.style.backgroundColor = "#fff";
-      txEl.innerText = `hash: ${txInfo.hash}`;
-      blockEl.appendChild(txEl);
-      const hash = txInfo.hash;
-      const param = `height=${block.height}`;
-      const txResponse = await fetch(`${base}/txs/${hash}/raw?${param}`);
-      const tx = await txResponse.json();
-      const link = `${exploreTx}/${hash}`;
-      try {
-        txEl.innerHTML = `<a href="${link}">${atob(tx.tx.body.memo)}</a>`;
-      } catch (e) {
-        txEl.innerHTML = `<a href="${link}">${tx.tx.body.memo}</a>`;
-      }
+      txs.data.forEach(async (txInfo) => {
+        const txEl = document.createElement("div");
+        txEl.id = txInfo.hash;
+        txEl.style.backgroundColor = "#fff";
+        txEl.innerText = `hash: ${txInfo.hash}`;
+        blockEl.appendChild(txEl);
+        const hash = txInfo.hash;
+        const param = `height=${block.height}`;
+        const txResponse = await fetch(`${base}/txs/${hash}/raw?${param}`);
+        const tx = await txResponse.json();
+        const link = `${exploreTx}/${hash}`;
+        try {
+          txEl.innerHTML = `<a href="${link}">${atob(tx.tx.body.memo)}</a>`;
+        } catch (e) {
+          txEl.innerHTML = `<a href="${link}">${tx.tx.body.memo}</a>`;
+        }
 
-      footerEl.innerText = "";
+        footerEl.innerText = "";
+      });
     });
-  });
+  } catch (e) {
+    footerEl.innerText = "error occured and dev is asleep";
+  }
 }
 
 window.onload = hunt;
