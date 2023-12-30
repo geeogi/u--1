a6bfbcd93116ca609567fad7b013bce7a80d3d53
geeogi <george6bennett@gmail.com>
2023-12-30 16:58:05 +0000

    add url

--- web/pages/el-operators/page-el-operators-build.js
+++ web/pages/el-operators/page-el-operators-build.js
@@ -1,5 +1,8 @@
 import * as fs from "fs";
 
+const url =
+  "https://goerli.eigenlayer.xyz/api/trpc/operator.getAllOperatorsWithMetadata,operator.getAllOperatorTVLNumStakers,native.userPodSummary,price.getPrices,native.globalPodSummary?batch=1&input=%7B%220%22%3A%7B%22json%22%3Anull%2C%22meta%22%3A%7B%22values%22%3A%5B%22undefined%22%5D%7D%7D%2C%221%22%3A%7B%22json%22%3Anull%2C%22meta%22%3A%7B%22values%22%3A%5B%22undefined%22%5D%7D%7D%2C%222%22%3A%7B%22json%22%3A%7B%22address%22%3A%22%22%7D%7D%2C%223%22%3A%7B%22json%22%3Anull%2C%22meta%22%3A%7B%22values%22%3A%5B%22undefined%22%5D%7D%7D%2C%224%22%3A%7B%22json%22%3A%7B%22staleTime%22%3A12500%2C%22refetchOnWindowFocus%22%3Afalse%7D%7D%7D";
+
 const HTML_TEMPLATE = fs.readFileSync(
   "./pages/el-operators/page-el-operators-template.html",
   "utf8"
