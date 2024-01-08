5b5253292fcd1703084a46472b2900d43903844a
geeogi <george6bennett@gmail.com>
2024-01-08 19:42:40 +0000

    fix asset location

--- web/build.js
+++ web/build.js
@@ -3,13 +3,12 @@ import { execSync } from "child_process";
 async function build() {
   execSync("rm -rf dist");
   execSync("mkdir dist");
-  execSync("cp -R assets/. dist/");
+  execSync("mkdir dist/assets");
+  execSync("cp -R assets/. dist/assets");
 
   await import("./pages/index/page-index-build.js");
   await import("./pages/eth-staking/page-staking-build.js");
   await import("./pages/celestia-hunter/page-celestia-hunter-build.js");
 }
 
-build()
-  .then(() => console.log("Build process completed."))
-  .catch((err) => console.error("An error occurred:", err));
+build().catch((err) => console.error("An error occurred:", err));
